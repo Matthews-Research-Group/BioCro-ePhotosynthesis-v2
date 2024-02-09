33a68070795bd5548dd1d29dceba78e44da07508
Yufeng <yufenghe87@gmail.com>
2024-02-08 21:33:18 -0600

    add constraint of protein content

--- enzyme_optimization/run_ephoto_opt.cpp
+++ enzyme_optimization/run_ephoto_opt.cpp
@@ -2,6 +2,8 @@
 #include <iomanip>
 #include <vector>
 #include <map>
+#include <sstream>
+#include <algorithm>
 
 #include <nlopt.hpp>
 
@@ -15,16 +17,12 @@
 #include "drivers/drivers.hpp"
 #include "Variables.hpp"
 
-#define number_of_parameters 26 
+#define number_of_parameters 25
 using namespace ePhotosynthesis;
 using namespace ePhotosynthesis::drivers;
 using namespace ePhotosynthesis::modules;
 
 const boost::regex token("\\s+");
-
-typedef struct {
-    double a, b;
-} my_constraint_data;
 
 struct EPS_inputs
 {
@@ -37,32 +35,48 @@ struct EPS_inputs
     double Tp = 25.0;
     double Ci = 300; 
     int maxSubSteps=2500;
+    double totalE=0.0;
+    std::vector<std::string> ename{}; 
+    std::vector<double> vmax{}; 
+    std::vector<double> kcat{}; 
+    std::vector<double> mweight{}; 
 };
 
 void print_map(const std::string& comment, const std::map<std::string, int>& m)
 {
   std::cout << comment;
-//    // Iterate using C++17 facilities
-//    for (const auto& [key, value] : m)
-//        std::cout << '[' << key << "] = " << value << "; ";
 
 // C++11 alternative:
   for (const auto& n : m)
   {
       std::cout << n.first << " = " << n.second << "; ";
   }
-
-// C++98 alternative:
-//  for (std::map<std::string, int>::const_iterator it = m.begin(); it != m.end(); ++it)
-//      std::cout << it->first << " = " << it->second << "; ";
-
-    std::cout << '\n';
+  std::cout << '\n';
 }
 
 void print(std::vector <double> const &a) {
    for(int i=0; i < a.size(); i++)
    std::cout << a.at(i) << ' ';
 }
+
+double myvconstraint(const std::vector<double> &parameters, std::vector<double> &grad, void *data)
+{
+    EPS_inputs *d = reinterpret_cast<EPS_inputs*>(data);
+    std::vector<double> vmax = d->vmax;
+    std::vector<double> kcat = d->kcat;
+    std::vector<double> mweight = d->mweight;
+    if (!grad.empty()) {
+       std::cout<<"grad should be empty!YH";
+       exit(3);
+    }
+    double constrain_E = 0.0;
+    for (int it = 0; it < parameters.size(); ++it) {
+      constrain_E += parameters[it]*vmax[it]/kcat[it]*mweight[it]/1000.0;
+    }
+    //take the difference between this totalE and the default total 
+    //std::cout<<"constrain_E "<<constrain_E;
+    return constrain_E - (d->totalE); 
+} 
 
 double EPS_run(const double& begintime, 
              const double& stoptime, 
@@ -73,6 +87,11 @@ double EPS_run(const double& begintime,
              const double& PAR,
              const double& Ci, 
              const int&    maxSubSteps,
+             const double& totalE,
+             const std::vector<std::string> &ename,
+             const std::vector<double> &vmax,
+             const std::vector<double> &kcat,
+             const std::vector<double> &mweight,
              const std::vector<double> &parameter_sf)
 {
        bool record = false;
@@ -84,19 +103,15 @@ double EPS_run(const double& begintime,
        readFile(evn, inputs);
        readFile(atpcost, inputs);
        Variables *theVars = new Variables();
-       readFile(enzymeFile, theVars->EnzymeAct);
-
-//       const int number_of_parameters = 26;//these enzymes will be scaled
+       //YH:readFile actually changes units by dividing 30!
+       readFile(enzymeFile, theVars->EnzymeAct); 
        int k=1;
-       for(auto& x:theVars->EnzymeAct)
+       for(auto& x:ename)
        {
-         if (k<=number_of_parameters)
-         {
-           x.second *= parameter_sf[k-1];
-         }
-         k++;
+          theVars->EnzymeAct.at(x) *= parameter_sf[k-1];
+//        std::cout<<x<<","<<theVars->EnzymeAct.at(x)<<std::endl;
+          k++;
        }
-
        //remove Ca and Light inputs from the text file. Instead, they are function inputs
        //theVars->TestCa = static_cast<double>(stof(inputs.at("CO2"), nullptr));
        //theVars->TestLi = static_cast<double>(stof(inputs.at("PAR"), nullptr));
@@ -145,29 +160,55 @@ double myvfunc(const std:: vector<double> &parameter_sf, std::vector<double> &gr
                    f_data_ptr->PAR,
                    f_data_ptr->Ci,
                    f_data_ptr->maxSubSteps,
+                   f_data_ptr->totalE,
+                   f_data_ptr->ename,
+                   f_data_ptr->vmax,
+                   f_data_ptr->kcat,
+                   f_data_ptr->mweight,
                    parameter_sf);
 }
 
 int main()
 {
-    EPS_inputs my_inputs;
-    
-    nlopt::opt opt(nlopt::GN_DIRECT, number_of_parameters);
-
+//read in txt inputs
+   EPS_inputs my_inputs;
+   std::ifstream infile;
+   infile.open("ProteinContentCal.txt");// 
+   std::string line;
+   while (std::getline(infile, line))
+   {
+       std::stringstream ss(line);
+       std::string name;
+       double vmax, kcat, mweight;
+       if (ss >> name>> vmax >> kcat >> mweight)
+       {
+          if(name=="V16") continue; // remove V16: ATP synthase
+          my_inputs.ename.push_back(name);
+          my_inputs.vmax.push_back(vmax);
+          my_inputs.kcat.push_back(kcat);
+          my_inputs.mweight.push_back(mweight);
+          my_inputs.totalE += vmax/kcat * mweight/1000.0; 
+       }
+   }
+   infile.close();// 
+   std::cout<<"default TotalE is"<<my_inputs.totalE<<"\n";
+//define optimizer    
+    nlopt::opt opt(nlopt::GN_ISRES, number_of_parameters);
 //lower and upper bounds
-    std::vector<double> lb(number_of_parameters,0.5);
-    std::vector<double> ub(number_of_parameters,2.0);
-
+    std::vector<double> lb(number_of_parameters,0.1);
+    std::vector<double> ub(number_of_parameters,10.0);
     opt.set_lower_bounds(lb);
     opt.set_upper_bounds(ub);
+//objective function
     opt.set_max_objective(myvfunc, &my_inputs);
-
+//constraints
+    opt.add_inequality_constraint(myvconstraint, &my_inputs, 1e-8);
+//tolerance for stopping
     opt.set_xtol_rel(1e-4);
-
 //initial guess
     std::vector<double> x(number_of_parameters,1.0);
     double maxf;
-    
+//run optimization    
     try{
         nlopt::result result = opt.optimize(x, maxf);
         std::cout << "found max at f("; 
