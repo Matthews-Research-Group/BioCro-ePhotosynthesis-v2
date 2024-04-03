56c0e81387f635827da3745d7e4a0f195b323e1c
uphone87 <yufenghe87@gmail.com>
2024-04-03 10:46:07 -0500

    add more plots

--- enzyme_optimization/fit_Aci/calibrate_ACi_and_AQ.R
+++ enzyme_optimization/fit_Aci/calibrate_ACi_and_AQ.R
@@ -27,3 +27,63 @@ for (i in 1:iteration_time){
   #update the scaling_factors for next iteration
   scaling_factors = aq[1:2]
 }
+
+#plot1: parameter evolution
+make_plot1<-function(){
+  par(mfrow=c(2,2),mar=c(4,4,3,3),mai=c(1,1,0.5,0.5),cex=1.5) #c(bottom, left, top, right)
+  linewidth=2
+  axislabelspace = 2
+  plot(fitting_results$step, fitting_results$Jmax,type='b',
+         lwd = linewidth,
+         xlab='',ylab='')
+  title(xlab = "iterations", line = axislabelspace)           # Add x-axis text
+  title(ylab = "Jmax", line = axislabelspace)                 # Add y-axis text
+  
+  plot(fitting_results$step, fitting_results$sf1,type='b',
+       lwd = linewidth,
+       xlab='',ylab='')
+  title(xlab = "iterations", line = axislabelspace)           # Add x-axis text
+  title(ylab = "sf (PhiPS2 base)", line = axislabelspace)                 # Add y-axis text
+  
+  plot(fitting_results$step, fitting_results$sf2,type='b',
+       lwd = linewidth,
+       xlab='',ylab='')
+  title(xlab = "iterations", line = axislabelspace)           # Add x-axis text
+  title(ylab = "sf (Theta base)", line = axislabelspace)                 # Add y-axis text
+  
+  plot(fitting_results$step, fitting_results$rmse,type='b',
+       lwd = linewidth,
+       xlab='',ylab='')
+  title(xlab = "iterations", line = axislabelspace)           # Add x-axis text
+  title(ylab = "RMSE", line = axislabelspace)                 # Add y-axis text
+}
+make_plot1()
+
+#plot2: compare new and old PhiPSII vs temperature curves
+library(ggplot2)
+make_plot2<-function(){
+  PhiPSII_func<-function(LeafTemperature,sf) {
+    return(0.352 *sf[1]+ 0.022 * sf[2]* LeafTemperature - sf[3]* 3.4*1e-4*LeafTemperature^2.0)
+  }
+  leafT = seq(0,40,by=5)
+  phiPS2_control = PhiPSII_func(leafT,c(1,1,1))
+  last_result = fitting_results[10,]
+  phiPS2_new  = PhiPSII_func(leafT,c(last_result$sf1,1,1))
+  df = data.frame(leafT,phiPS2_control,phiPS2_new)
+  df_for_plot = reshape2:: melt(df,id.vars="leafT")
+  myplot<-ggplot(data=df_for_plot,aes(x=leafT, y=value,colour=variable)) +
+    geom_line()  +  geom_point() + ylab("PhiPS2")
+  myplot
+}
+make_plot2()
+
+#plot3: compare ACi and AQ curves between OBS and newly calibrated Farquhar.
+last_result = fitting_results[10,]
+scaling_factors = c(last_result$sf1,last_result$sf2)
+source('compare_ACi_with_obs.R')
+p1=compare_ACi_with_obs(year,last_result$Vcmax,last_result$Jmax,last_result$TPU,scaling_factors)
+source('compare_AQ_with_obs.R')
+p2=compare_AQ_with_obs(year,last_result$Vcmax,last_result$Jmax,last_result$TPU,scaling_factors)
+require(gridExtra)
+grid.arrange(p1, p2, ncol=2)
+
