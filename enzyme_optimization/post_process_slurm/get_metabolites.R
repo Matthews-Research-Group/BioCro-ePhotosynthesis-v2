b1537528bd74344e6f11d662440e82aac9d133c5
Yufeng <yufenghe87@gmail.com>
2024-07-10 09:33:46 -0500

    [post] simulate control A

--- enzyme_optimization/post_process_slurm/get_metabolites.R
+++ enzyme_optimization/post_process_slurm/get_metabolites.R
@@ -1,6 +1,6 @@
 library(stringr)
 # Function to read the second-to-last line from a text file
-read_second_to_last_line <- function(file_path) {
+read_last_two_lines <- function(file_path) {
   # Read all lines from the file into a character vector
   lines <- readLines(file_path)
   
@@ -11,7 +11,9 @@ read_second_to_last_line <- function(file_path) {
   
   # Return the second-to-last line
   second_to_last_line <- lines[length(lines) - 1]
-  return(second_to_last_line)
+  # Return the last line
+  last_line <- lines[length(lines)]
+  return(c(second_to_last_line,last_line))
 }
 # Function to extract the number after "Penalty is" from a line
 extract_penalty <- function(line) {
@@ -24,7 +26,8 @@ extract_penalty <- function(line) {
     penalty_value <- as.numeric(sub("Penalty is\\s*", "", penalty))
     return(penalty_value)
   } else {
-    stop("No match found for 'Penalty is'.")
+    #stop("No match found for 'Penalty is'.")
+    return(NaN) 
   }
 }
 
@@ -34,34 +37,65 @@ extract_penalty <- function(line) {
 #here we use LD_PRELOAD specifically for R sessions to prioritize libraries from Conda
 Sys.setenv(LD_PRELOAD = "/home/n-z/yufenghe/.conda/envs/ephoto/lib/libstdc++.so.6")
 
-para_best_all = readRDS("para_best_all_Rep1.rds")
-rep = 1
-output_matrix = c()
-for (i in 1:nrow(para_best_all)){
-    print(paste("processing",i))
-    para_best = para_best_all[i,]
-    r = para_best[1]
-    t = para_best[2]
-    c = para_best[3]
-    
-    opt_scaling_factors = para_best[4:28]
+reps = 1:5 #name the case
+rerun= FALSE #is it a rerun?
+for (rep in reps){
+  #get the inputs of parameters of scaling factors
+  if(rerun){
+    para_best_all = readRDS(paste0("para_best_reRun_Rep",rep,".rds"))
+  }else{
+    para_best_all = readRDS(paste0("para_best_all_Rep",rep,".rds"))
+  }
+  output_matrix = c()
+  Actl = c()
+  for (i in 1:nrow(para_best_all)){
+      print(paste("processing rep",rep,"case",i))
+      para_best = para_best_all[i,]
+  
+      r = para_best[1]
+      t = para_best[2]
+      c = para_best[3]
 
-    write(opt_scaling_factors, 
-                file = "optimized_enzyme_scaling_factors.txt",
-          ncolumns = length(opt_scaling_factors), sep = " ")
-
-    cmd = paste0("./ephoto_single.exe -r ",r," -t ",t," -c ",c," >& log")
-    print(cmd)
-    system(cmd)
-    Sys.sleep(2)
-    penalty_line = read_second_to_last_line("log")
-    penalty      = extract_penalty(penalty_line)
-    last_metabolite = read.table("last_data.txt")
-    output = c(r,t,c,penalty,last_metabolite)
-    output_matrix = rbind(output_matrix,output)
+#run optimized case
+      opt_scaling_factors = para_best[4:28]
+      write(opt_scaling_factors, 
+                  file = "optimized_enzyme_scaling_factors.txt",
+            ncolumns = length(opt_scaling_factors), sep = " ")
+      cmd = paste0("./ephoto_single.exe -r ",r," -t ",t," -c ",c," >& log")
+      print(cmd)
+      system(cmd)
+      Sys.sleep(2)
+      two_lines = read_last_two_lines("log")
+      penalty_line = two_lines[1] 
+      penalty      = extract_penalty(penalty_line)
+      Avalue       = as.numeric(two_lines[2])
+      last_metabolite = read.table("last_data.txt")
+      if(is.nan(penalty)) last_metabolite[] = NaN
+      output = c(r,t,c,penalty,last_metabolite)
+      output_matrix = rbind(output_matrix,output)
+#run control
+#control needs to be run only once without rerun
+      if(!rerun){
+       opt_scaling_factors[] = 1 
+       write(opt_scaling_factors, 
+                   file = "optimized_enzyme_scaling_factors.txt",
+             ncolumns = length(opt_scaling_factors), sep = " ")
+       cmd = paste0("./ephoto_single.exe -r ",r," -t ",t," -c ",c," >& log")
+       system(cmd)
+       Sys.sleep(2)
+       two_lines = read_last_two_lines("log")
+       Actl       = c(Actl,as.numeric(two_lines[2]))
+      }
+  }
+  colnames(output_matrix)[1] = "Q"
+  colnames(output_matrix)[2] = "T"
+  colnames(output_matrix)[3] = "Ci"
+  colnames(output_matrix)[4] = "Penalty"
+  if(rerun){
+    saveRDS(output_matrix,paste0("steady_state_metabolite_rep",rep,"_rerun.rds"))
+  }else{
+    saveRDS(output_matrix,paste0("steady_state_metabolite_rep",rep,".rds"))
+    para_best_all_with_control = cbind(para_best_all,Actl)
+    saveRDS(para_best_all_with_control,paste0("para_best_all_Rep",rep,"_withCTL.rds"))
+  }
 }
-rownames(output_matrix)[1] = "Q"
-rownames(output_matrix)[2] = "T"
-rownames(output_matrix)[3] = "Ci"
-rownames(output_matrix)[4] = "Penalty"
-saveRDS(output_matrix,paste0("steady_state_metabolite_rep",rep,".rds"))
