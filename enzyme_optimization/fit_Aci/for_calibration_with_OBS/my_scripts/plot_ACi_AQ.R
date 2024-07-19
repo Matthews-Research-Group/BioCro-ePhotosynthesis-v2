106c202978e39e34e09ef8c2104d9dc034aa3651
uphone87 <yufenghe87@gmail.com>
2024-07-18 23:16:22 -0500

    [fit aci] update plotting script

--- enzyme_optimization/fit_Aci/for_calibration_with_OBS/my_scripts/plot_ACi_AQ.R
+++ enzyme_optimization/fit_Aci/for_calibration_with_OBS/my_scripts/plot_ACi_AQ.R
@@ -4,8 +4,8 @@ library(ggplot2)
 library(reshape2)
 source("biocro_FvCB.R")
 
-plot_type = 2 #1:ACi; 2: AQ
-year = 2022
+plot_type = 1 #1:ACi; 2: AQ
+year = 2021
 
 if(plot_type == 1){
   parameters <- read.csv.exdf("ld11_aci_fit_parameters.csv")
@@ -49,13 +49,16 @@ if(plot_type == 1){
   
   df_sub = df_melt[ grep(paste0("^",year), df_melt$curve_identifier),]
   
-  ggplot(df_sub, aes(x = Ci, y = value, color = curve_identifier, linetype = variable,shape = variable)) +
+  p<-ggplot(df_sub, aes(x = Ci, y = value, color = curve_identifier, linetype = variable,shape = variable)) +
     geom_line() + geom_point() +
     labs(x = "Ci (ppm)", 
-         y = bquote(An~(mu*mol ~ m^-2 ~ s^-1))
+         y = bquote(An~(mu*mol ~ m^-2 ~ s^-1)),
+         title = paste("year",year)
          ) +
     theme_bw() +
-    theme(text = element_text(size = 20),legend.position = c(0.8, 0.2))
+    theme(text = element_text(size = 20),legend.position = c(0.8, 0.3))
+  
+  ggsave(paste0("ACi_",year,".pdf"),p,width=8,height=8)
   
 }else if(plot_type == 2){
 
@@ -85,6 +88,7 @@ if(plot_type == 1){
   
   df_sub = df_melt[ grep(paste0("^",year), df_melt$curve_identifier),]
   
+  #plot individual lines
   ggplot(df_sub, aes(x = Q, y = value, color = curve_identifier, linetype = variable,shape = variable)) +
     geom_line() + geom_point(size=3) +
     labs(x = bquote(Q~(mu*mol ~ m^-2 ~ s^-1)), 
@@ -93,5 +97,31 @@ if(plot_type == 1){
     theme_bw() +
     theme(text = element_text(size = 20),legend.position = c(0.8, 0.2))
   
-  
+  #plot average line
+  unique_identifier = unique(df$curve_identifier)
+  xx = 0
+  A_modelled = c()
+  A_observed = c()
+  for (id in unique_identifier){
+    tmp = df[df$curve_identifier == id,1:3]
+    xx = xx + tmp
+    A_modelled = cbind(A_modelled,tmp$Modelled)
+    A_observed = cbind(A_observed,tmp$Observed)
+  }
+  sd_modelled = apply(A_modelled,1,sd)
+  sd_observed = apply(A_observed,1,sd)
+  df_avg = xx/length(unique_identifier)
+  df_melt = melt(df_avg,id.vars = c("Q"),variable.name = "variable", value.name = "value")
+  df_melt = cbind(df_melt,sd = c(sd_observed,sd_modelled))
+  ggplot(df_melt, aes(x = Q, y = value, color = variable)) +
+    geom_line() + geom_point(size=3) +
+    scale_color_manual(values=c("black", "blue"))+
+    geom_errorbar(aes(ymin=value-sd, ymax=value+sd), width=.2,
+                  position=position_dodge(0.05)) +
+    labs(x = bquote(Q~(mu*mol ~ m^-2 ~ s^-1)), 
+         y = bquote(An~(mu*mol ~ m^-2 ~ s^-1))
+    ) +
+    title
+    theme_bw() +
+    theme(text = element_text(size = 20),legend.position = c(0.8, 0.2))
 }
