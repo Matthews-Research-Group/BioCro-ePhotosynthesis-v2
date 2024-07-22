1e377f0bb83575f915d8e474cd0cd5925fd02019
uphone87 <yufenghe87@gmail.com>
2024-07-22 12:18:15 -0500

    [fit aci] update run_ACi_AQ_AT.R

--- enzyme_optimization/fit_Aci/run_ACi_AQ_AT.R
+++ enzyme_optimization/fit_Aci/run_ACi_AQ_AT.R
@@ -1,42 +1,34 @@
 library(reshape2)
 library(ggplot2)
-source("for_calibration_with_OBS/FarquharModel.R")
-alpha1 = 0.87
-alpha2 = 0.98
-curve_option = 3 #1: A-Ci;2: A-Q; 3: A-T 
-output_figure_name = "figs/AT_Q2000_r4.pdf"
+library(BioCro)
+source("for_calibration_with_OBS/my_scripts/biocro_FvCB.R")
+alpha1_alpha2 = read.csv('ePhotosynthesis_optimal_alpha1_alpha2.csv')
+alpha1 = alpha1_alpha2[2]
+alpha2 = alpha1_alpha2[3]
+curve_option = 1 #1: A-Ci;2: A-Q; 3: A-T 
+output_figure_name = "figs/ACi_Q400_v1.pdf"
 #call ephoto c++
-system(paste("./myephoto.exe",alpha1,alpha2,curve_option))
+system(paste("./myephoto_PAR400.exe",alpha1,alpha2,curve_option))
 #read in ephoto results
 ephoto = read.csv("output.data",header=FALSE)
 colnames(ephoto) = c("PAR","Tleaf","Ci","An")
 
+aci_fit_results <- read.csv("for_calibration_with_OBS/my_scripts/ld11_aci_fit_parameters_avg.csv")
+Vcmax25      <- aci_fit_results$mean[aci_fit_results$parameter=="Vcmax_at_25"]
+Jmax25       <- aci_fit_results$mean[aci_fit_results$parameter=="Jmax_at_25"]
+Rd25         <- 1.28
+TPU25        <- aci_fit_results$mean[aci_fit_results$parameter=="TPU_at_25"]
 #run Farquhar
 An_farquhar = NA*(1:dim(ephoto)[1])
-Aj_farquhar = An_farquhar
-Ac_farquhar = An_farquhar
-Ap_farquhar = An_farquhar
 for (i in 1:dim(ephoto)[1]){
-  R=8.314472E-3 #Gas constant KJ mole^{-1} K^{-1}
-  Rd25   = 1.28 
-  Air_O2 = 210  #
-  
-  Vcmax25  = 123   #LD11
-  Jmax25   = 216   #LD11
-  Rate_TPu = 14.6  #LD11
-  scaling_factors = c(0.72, 0.64)
   #For Farquhar, we need the total Q, as there's a calculation of
-  #absorption in the Farquhar function
-  output_farquhar  = FarquharModel(ephoto$Tleaf[i], ephoto$Ci[i], ephoto$PAR[i], Air_O2, Vcmax25, Jmax25, Rate_TPu,scaling_factors)
-  
-  Rd = Rd25 * exp(18.72 - 46.39 / (R * (ephoto$Tleaf[i]+ 273.15)))
-  An_farquhar[i]   = output_farquhar$GA - Rd
-  Aj_farquhar[i]   = output_farquhar$Aj
-  Ac_farquhar[i]   = output_farquhar$Ac
-  Ap_farquhar[i]   = output_farquhar$Ap
+  #absorption inside the Farquhar function
+  output_farquhar  = BioCro_FvCB(ephoto$PAR[i],ephoto$Tleaf[i], ephoto$Ci[i], Vcmax25, Jmax25, Rd25, TPU25)
+  An_farquhar[i]   = output_farquhar$An
 }
 
-df_for_plot = cbind(ephoto,An_Farquhar=An_farquhar)
+df_for_plot = cbind(ephoto,An_FvCB=An_farquhar)
+colnames(df_for_plot)[colnames(df_for_plot)=="An"] = "An_ePhoto"
 df_for_plot = melt(df_for_plot, id.vars=c("PAR","Tleaf","Ci"))
 
 if(curve_option==1){
