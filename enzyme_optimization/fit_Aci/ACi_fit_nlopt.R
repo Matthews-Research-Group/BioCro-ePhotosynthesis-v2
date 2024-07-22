81fe188c1ae18424407c86a53493708f0657eda3
uphone87 <yufenghe87@gmail.com>
2024-07-22 11:52:44 -0500

    [fit aci] remove hard-coded Vcmax25 etc.

--- enzyme_optimization/fit_Aci/ACi_fit_nlopt.R
+++ enzyme_optimization/fit_Aci/ACi_fit_nlopt.R
@@ -12,9 +12,15 @@ source('for_calibration_with_OBS/my_scripts/biocro_FvCB.R') #Vcmax_multiplier fu
 source('aci_functions.R')
 
 obj_func<-function(x){
-  vcmax_target = 134.5
-  jmax_target  = 183
-  tpu_target   = 10.5
+#  vcmax_target = 134.5
+#  jmax_target  = 183
+#  tpu_target   = 10.5
+  # Specify FvCB parameter values
+  aci_fit_results <- read.csv("for_calibration_with_OBS/my_scripts/ld11_aci_fit_parameters_avg.csv")
+  vcmax_target <- aci_fit_results$mean[aci_fit_results$parameter=="Vcmax_at_25"]
+  jmax_target  <- aci_fit_results$mean[aci_fit_results$parameter=="Jmax_at_25"]
+  Rd_at_25     <- 1.28
+  tpu_target   <- aci_fit_results$mean[aci_fit_results$parameter=="TPU_at_25"]
   system(paste("./myephoto.exe",x[1],x[2],1))
   ePhoto_result = read.table("output.data",header=FALSE,sep = ",")
   A_Ci_df = as.data.frame(ePhoto_result)
@@ -24,12 +30,13 @@ obj_func<-function(x){
   jmax  = output[2]
   tpu   = output[3]
   
-  rmse = sqrt(((vcmax - vcmax_target)^2 + (jmax - jmax_target)^2 + (tpu - tpu_target)^2)/3)
-  return(rmse)
+  error = (vcmax - vcmax_target)^2 + (jmax - jmax_target)^2 #+ (tpu - tpu_target)^2
+  if(is.na(error)) error=999
+  return(error)
 }
 
 lbs = c(0.8,0.9) #alpha1, alpha2 
-ubs = c(1.0,1.1)
+ubs = c(1.0,1.2)
 init_guess = rep(1,length(lbs))
 
 opt_results = nloptr(x0 = init_guess, eval_f = obj_func, 
@@ -38,4 +45,5 @@ opt_results = nloptr(x0 = init_guess, eval_f = obj_func,
                      opts = list("algorithm" = "NLOPT_LN_SBPLX","xtol_rel"=1.0e-6))
 alpha1_alpha2 = as.data.frame(opt_results$solution)
 rownames(alpha1_alpha2) = c('alpha1','alpha2')
+alpha1_alpha2 = t(alpha1_alpha2)
 write.csv(alpha1_alpha2,"ePhotosynthesis_optimal_alpha1_alpha2.csv")
